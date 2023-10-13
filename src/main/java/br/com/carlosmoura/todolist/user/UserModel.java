5a2b833c888d63be1db2c615e2c058a61f507ed4
Carlos Eduardo <carloseduardo.moura.contato@gmail.com>
2023-10-13 18:48:14 -0300

    dados criptografados

--- src/main/java/br/com/carlosmoura/todolist/user/UserModel.java
+++ src/main/java/br/com/carlosmoura/todolist/user/UserModel.java
@@ -1,27 +1,31 @@
 package br.com.carlosmoura.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity (name = "tb_users")
+@Data
 public class UserModel {
 
+@Id
+@GeneratedValue(generator = "UUID")
+private UUID id;
+
+@Column(unique = true)
 private String username;
 private String name;
-private String password;
+private String password;    
 
-public void setUsername(String username) {
-    this.username = username;
-}
-public String getUsername() {
-    return username;
-}
-public void setName(String name) {
-    this.name = name;
-}
-public String getName() {
-    return name;
-}
-public void setPassword(String password) {
-    this.password = password;
-}
-public String getPassword() {
-    return password;
-}
+@CreationTimestamp
+private LocalDateTime createdAt;
+
+
 }
